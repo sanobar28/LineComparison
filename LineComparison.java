972ca917690f14fff81bd2b075e85727e30f312d
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-01 16:57:35 +0530

    length of line calculated

--- LineComparison.java
+++ LineComparison.java
@@ -1,9 +1,16 @@
+import java.lang.Math;
+
 public class LineComparison{
 
 	public static void main(String[] args){
 	
-		System.out.println("Welcome to line comparison pgm");
+		//System.out.println("Welcome to line comparison pgm");
+		
+		int x1=20, y1=15, x2=22, y2=17;
+		double length = Math.sqrt((x2-x1)^2 + (y2-y1));
 
+ 		System.out.println("Length of line: " + length);
+		
 	}
 
 }
