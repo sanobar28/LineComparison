fa1d56646f90ed86710109fa89a169ecdcc579a8
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-01 19:08:40 +0530

    Line comparison using equals to

--- LineComparison.java
+++ LineComparison.java
@@ -1,16 +1,49 @@
 import java.lang.Math;
+import java.util.Scanner;
 
 public class LineComparison{
 
 	public static void main(String[] args){
 	
 		//System.out.println("Welcome to line comparison pgm");
-		
-		int x1=20, y1=15, x2=22, y2=17;
-		double length = Math.sqrt((x2-x1)^2 + (y2-y1));
+		Scanner s = new Scanner(System.in);
+	         //getting values from user
+	         System.out.println("Enter the first co-ordinate of x of line 1");
+	         int x1 = s.nextInt();
+	         System.out.println("Enter the first co-ordinate of y of line 1");
+	         int y1 = s.nextInt();
+	         System.out.println("Enter the second co-ordinate of x of line 1");
+	         int x2 = s.nextInt();
+	         System.out.println("Enter the second co-ordinate of y of line 1");
+	         int y2 = s.nextInt();
+	         //operation for finding length of line
+	         double length_line1 = Math.sqrt((Math.pow((x2 - x1) , 2 )+Math.pow((y2 - y1) , 2 )));
+	         //printing first length of line
+	         System.out.println("The length of line 1 is" +length_line1);
 
- 		System.out.println("Length of line: " + length);
-		
+	         //entering value for line 2
+	         System.out.println("Enter the first co-ordinate of x of line 2");
+	         int X1 = s.nextInt();
+	         System.out.println("Enter the first co-ordinate of y of line 2");
+	         int Y1 = s.nextInt();
+	         System.out.println("Enter the second co-ordinate of x of line 2");
+	         int X2 = s.nextInt();
+	         System.out.println("Enter the second co-ordinate of y of line 2");
+	         int Y2 = s.nextInt();
+	         //operations for line 2 length
+	         double length_line2 = Math.sqrt((Math.pow((X2 - X1) , 2 ) + Math.pow((Y2 - Y1) , 2 )));
+	         //displaying result
+	         System.out.println("The length of line 2 is " +length_line2);
+	         //comparing line 1 and line2
+	         if( length_line1 == length_line2 )
+	         {
+	             System.out.println("lines are equal");
+	         }
+	         else
+	         {
+	             System.out.println("lines are not equal");
+	         }
 	}
-
+		
 }
+
